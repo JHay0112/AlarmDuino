91aaccf15809e86fb755e0d1f7972ebcd53e91b4
Jordan Hay <jordanhay0112@gmail.com>
2020-09-16 21:43:51 +1200

    Init Commit

---
+++ AlarmDuino.ino
@@ -0,0 +1,13 @@
+// Constants
+#define ALARM_PIN 13
+
+// Main
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
